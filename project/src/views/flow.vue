7f6f614b0bbde006219914def3b853e808c45925
Sirius.Black <ofghrt@qq.com>
2019-05-13 23:22:43 +0800

    update

--- project/src/views/flow.vue
+++ project/src/views/flow.vue
@@ -1,9 +1,11 @@
 <template>
 	<div class="flow">
-		<img src="../assets/flow/1.png" class="flowSection" alt="" @click="flow(4)">
-		<img src="../assets/flow/2.png" class="flowSection" alt="" @click="flow(1)">
+		<img src="../assets/flow/5.png" class="flowSection" alt="" @click="flow(5)" style="margin-top: 25%;">
 		<img src="../assets/flow/3.png" class="flowSection" alt="" @click="flow(2)">
+		<img src="../assets/flow/6.png" class="flowSection" alt="" @click="flow(6)">
+		<img src="../assets/flow/1.png" class="flowSection" alt="" @click="flow(4)">
 		<img src="../assets/flow/4.png" class="flowSection" alt="" @click="flow(3)" style="margin-top: 15px">
+		<!-- <img src="../assets/flow/2.png" class="flowSection" alt="" @click="flow(1)"> -->
 	</div>
 </template>
 
@@ -26,6 +28,10 @@
 					this.$router.push({path: "/album"})
 				if (index === 4)
 					window.location.href = 'https://e.eqxiu.com/s/JfEEUI2P';
+				if (index === 5)
+					window.location.href = 'https://x.eqxiu.com/s/GEdtovuT';
+				if (index === 6)
+					window.location.href = 'https://q.eqxiu.com/s/NfWDwoiU';
 				// this.$router.replace( { path : "/flowIntro"})
 			}
 		}
@@ -38,7 +44,7 @@
 		background: url('../assets/flow/bg.png') round;
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
+		justify-content: flex-start;
 		align-items: center
 	}
 
@@ -55,6 +61,6 @@
 		width: 80%;
 		height: auto;
 		display: block;
-		margin-bottom: 20px;
+		margin-bottom: 10px;
 	}
 </style>
